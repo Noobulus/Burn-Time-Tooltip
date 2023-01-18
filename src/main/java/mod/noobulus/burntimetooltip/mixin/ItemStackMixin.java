e95cfae634d2f478ced3b0d766c864581e66e99f
Noobulus <mattmattiasmatthew@gmail.com>
2023-01-18 13:29:23 -0600

    (hopefully) make things break less with other mods and remove some unused code

--- src/main/java/mod/noobulus/burntimetooltip/mixin/ItemStackMixin.java
+++ src/main/java/mod/noobulus/burntimetooltip/mixin/ItemStackMixin.java
@@ -16,26 +16,32 @@ import java.util.List;
 
 import static net.minecraft.block.entity.AbstractFurnaceBlockEntity.createFuelTimeMap;
 
-@Mixin(ItemStack.class)
+@Mixin(value = ItemStack.class, priority = 42069)
 public class ItemStackMixin {
 
     private final int DIVISOR_TICKS = 1;
     private final int DIVISOR_SECONDS = 20;
     private final int DIVISOR_ITEMS = 200;
 
-    @Inject(method = "getTooltip", at = @At(value = "RETURN"), cancellable = true)
+    @Inject(method = "getTooltip", at = @At(value = "RETURN"))
     private void onGetTooltip(PlayerEntity player, TooltipContext context, CallbackInfoReturnable<List<Text>> cir) {
         List<Text> tooltip = cir.getReturnValue();
         float burnTime = getBurnTime((ItemStack) (Object) this, DIVISOR_ITEMS);
         if (burnTime > 0) {
             tooltip.add(Text.translatable("tooltip.burntimetooltip.burntime",
-                    Text.literal(String.valueOf(burnTime))).setStyle(Style.EMPTY.withColor(Formatting.GRAY)));
+                    Text.literal(formatNumber(burnTime))).setStyle(Style.EMPTY.withColor(Formatting.GRAY)));
         }
-        cir.setReturnValue(tooltip);
     }
 
     private float getBurnTime(ItemStack fuel, int divisor) {
         Item item = fuel.getItem();
         return (float)createFuelTimeMap().getOrDefault(item, 0) / divisor;
+    }
+
+    private String formatNumber (float num) {
+        if (num % 1 == 0) {
+            return String.valueOf(((int)num));
+        }
+        return String.valueOf(num);
     }
 }
